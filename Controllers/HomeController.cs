4be40b4a2463670b9b3f53892d1e072c2a142b7a
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2023-03-17 18:18:07 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Split_Receipt.Areas.Identity.Data;
+using Split_Receipt.Models;
+using System.Diagnostics;
+
+namespace Split_Receipt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            this._userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["userId"] = _userManager.GetUserId(this.User);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
